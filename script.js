1f550f59500051b8c769f470bb293cbd18fe9df2
Sankari Karthik <sankarikarthik03@gmail.com>
2022-07-01 17:21:48 +0530

    Implement logic to change dimensions of grid

--- script.js
+++ script.js
@@ -1,23 +1,42 @@
 // creating the square grid
 let containerDiv = document.querySelector('.container');
-for(let i = 0; i < 16; i++) {
-    let row = document.createElement("div");
-    containerDiv.appendChild(row);
-    row.style.display = 'flex';
-    for (let j = 0; j < 16; j++) {
-        const node = document.createElement("div");
-        node.style.backgroundColor = "red";
-        node.classList.add('element');
-        //node.style.margin = "1px";
-        node.style.height = "20px";
-        node.style.width = "20px";
-        row.appendChild(node);
-    }
-}
+const LENGTH = 600;
+containerDiv.style.height = "600px";
+containerDiv.style.width = "600px";
 
-let divs = document.querySelectorAll('.element');
-divs.forEach((div) => {
+let submit = document.querySelector('button');
+ 
+let dimensions;
+submit.addEventListener('click', () => {
+    // Erase previous grid if exists
+    while (containerDiv.firstChild) {
+        containerDiv.removeChild(containerDiv.lastChild);
+      }
+    // Create new grid
+    dimensions = document.querySelector('input').value;
+    console.log(dimensions);
+    for(let i = 0; i < dimensions; i++) {
+        let row = document.createElement("div");
+        containerDiv.appendChild(row);
+        row.style.display = 'flex';
+        for (let j = 0; j < dimensions; j++) {
+
+            const node = document.createElement("div");
+            node.style.backgroundColor = "#3caea3";
+            node.classList.add('element');
+            //node.style.margin = "1px";
+            node.style.height = `${LENGTH/dimensions}px`;
+            node.style.width = `${LENGTH/dimensions}px`;
+            row.appendChild(node);
+        }
+    }
+    // Hover over the cells
+    let divs = document.querySelectorAll('.element');
+    divs.forEach((div) => {
     div.addEventListener('mouseover', () => {
-        div.style.backgroundColor = 'blue';
+        div.style.backgroundColor = 'bisque';
     });
 });
+
+})
+
